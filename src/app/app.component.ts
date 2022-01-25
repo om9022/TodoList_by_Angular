223c1e497f41afb069ea50b2170f67d9163c1c26
om9022 <omkarchikane607@gmail.com>
2022-01-26 01:17:41 +0530

    Innitial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'om-todo-list';
+  constructor(){
+    // setTimeout(() =>{
+    //   this.title = "change title";
+    // }, 4000)
+  }
 }
